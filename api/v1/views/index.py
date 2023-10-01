846a19ea5bc31576add701b5e8d956eaa67bd5c5
Michael Machohi <111045570+sierra-95@users.noreply.github.com>
2023-10-01 14:47:46 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
-"""
-Flask route that returns json status response
-"""
+""" Index Module """
+
+
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "City": City, "Place": Place,
+           "Review": Review, "State": State, "User": User}
+plurals = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """
-    function for status route that returns the status
-    """
-    if request.method == 'GET':
-        resp = {"status": "OK"}
-        return jsonify(resp)
+@app_views.route("/status")
+def get_status():
+    """ Returns a status in JSON format """
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    """
-    function to return the count of all class objects
-    """
-    if request.method == 'GET':
-        response = {}
-        PLURALS = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in PLURALS.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+@app_views.route("/stats")
+def num_of_objs():
+    """ Retrieves the number of each object by type """
+    totals = {}
+    for key, value in classes.items():
+        for k, v in plurals.items():
+            if value == v:
+                totals[k] = storage.count(key)
+    return jsonify(totals)
